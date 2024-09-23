54338c30260e97ab6bbaac5d54a5a6eae793856c
Cindy Ngo <62029045+cindyngo44@users.noreply.github.com>
2024-09-23 15:19:45 -0400

    starter code

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'https://cindyngo44.github.io',
+});
